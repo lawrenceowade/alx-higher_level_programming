f7c60f291e64696baeee04360d1c5e419e0ad844
lawrenceowade <larryowade0@gmail.com>
2023-08-10 14:30:08 -0700

    Copy - Cut - Paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-print(f"First 3 letters:{}".format(word_first_3))
-print(f"Last 2 letters:{}".format(word_last_2))
-print(f"Middle word:{}".format (middle_word))
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
