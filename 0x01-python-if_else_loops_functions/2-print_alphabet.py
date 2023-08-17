8a89844acc6b244ab3d19231b9b61cb2334dcb84
lawrenceowade <larryowade0@gmail.com>
2023-08-17 01:56:52 -0700

    alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 alphabet = ""
 for i in range(ord('a'), ord('z')+1):
-    alphabet += "{}".format(chr(i))
+    alphabet += chr(i)
 
-print(alphabet)
+print(alphabet)  # Print the alphabet string
 
