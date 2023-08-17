d62cc014ace451b41804b331efc22419005f5e86
lawrenceowade <larryowade0@gmail.com>
2023-08-17 04:10:36 -0700

    moled

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
-from add_0 import add
+def main():
+    from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    result = add(a, b)
+
+    print(f"{a} + {b} = {result}")
+
+if __name__ == "__main__":
+    main()
 
